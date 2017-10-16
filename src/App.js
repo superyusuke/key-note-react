5dd87cd05693b7f039c2e88de7771e026e130fae
NAKANISHI Yusuke <nakanishiyusuke0000@gmail.com>
2017-10-17 00:38:57 +0900

    不要なファイルの削除

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.css';
 import Keyboard from './components/Keyboard'
 
 class App extends Component {
