c2e643501a0d7575f46643eeabde4ac5800c9179
NAKANISHI Yusuke <nakanishiyusuke0000@gmail.com>
2017-10-17 00:36:21 +0900

    Keyboard コンポーネントの作成と配置

---
+++ src/components/Keyboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Keyboard.css'
+
+const Keyboard = () => {
+  return (
+    <div className={styles.red}>
+      <ul>
+        <li className={styles.blue}>fjfjj</li>
+        <li>fjfjj</li>
+        <li>fjfjj</li>
+        <li>fjfjj</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Keyboard
