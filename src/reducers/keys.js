7dec9c7fd89c3ebbca50815d9472ecf2b746cead
NAKANISHI Yusuke <nakanishiyusuke0000@gmail.com>
2017-10-17 08:48:13 +0900

    key の初期ステイトの変更

--- src/reducers/keys.js
+++ src/reducers/keys.js
@@ -1,19 +1,8 @@
-const octave = [
-  true,
-  false,
-  true,
-  false,
-  true,
-  true,
-  false,
-  true,
-  false,
-  true,
-  false,
-  true
+const defaultKeys = [
+  0,1,2,3,4,5,6,7,8,9,10,11
 ]
 
-const keys = (state = octave, action) => {
+const keys = (state = defaultKeys, action) => {
   switch (action) {
     case true:
       return true
